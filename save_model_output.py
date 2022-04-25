e6032ecb442b8ffb8180b9a250ed67b0886eca84
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2022-04-24 22:49:49 -0700

    revise comments and clean up code more

--- save_model_output.py
+++ save_model_output.py
@@ -3,7 +3,6 @@ import datetime
 import boto3
 
 OUTPUT_DIRS = ["./model/outputs/flow", "./model/outputs/temperature"]
-BACKEND_URL = "http://127.0.0.1:8000/input_to_db"
 
 # Send new predictions to S3
 def save_model_output():
@@ -12,26 +11,23 @@ def save_model_output():
     bucket = s3.Bucket("lake-tahoe-conditions")
 
     today = datetime.datetime.now(datetime.timezone.utc)
-    # TODO: possibly zip these files so we only need to send one request
     for dir in OUTPUT_DIRS:
         for fileName in os.listdir(dir):
+            # Only upload new predictions
             fileDate = datetime.datetime.strptime(fileName, "%Y-%m-%d %H.npy")
             # TODO: use PST to avoid confusion?
             fileDate = fileDate.replace(tzinfo=datetime.timezone.utc)
             if fileDate <= today:
-                # Assume old predictions already in S3
                 continue
 
             # Upload new prediction to S3
-            # Determine object key
+            localFilePath = f"{dir}/{fileName}"
             if dir.endswith("flow"):
                 objKey = f"flow/{fileName}"
             else:
                 objKey = f"temperature/{fileName}"
-
-            filePath = f"{dir}/{fileName}"
             # TODO: handle upload errors
-            bucket.upload_file(filePath, objKey)
+            bucket.upload_file(localFilePath, objKey)
 
 if __name__ == '__main__':
     save_model_output()
