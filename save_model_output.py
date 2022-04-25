648e87f4b7ffc69b94cf691aa6d52c628cb8c1e5
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2022-04-24 22:47:26 -0700

    refactor save_model_output

--- save_model_output.py
+++ save_model_output.py
@@ -1,38 +1,37 @@
 import os
 import datetime
-import requests
-import time
 import boto3
 
 OUTPUT_DIRS = ["./model/outputs/flow", "./model/outputs/temperature"]
 BACKEND_URL = "http://127.0.0.1:8000/input_to_db"
 
+# Send new predictions to S3
 def save_model_output():
-    today = datetime.datetime.now(datetime.timezone.utc)
-
     # S3 bucket
     s3 = boto3.resource("s3")
     bucket = s3.Bucket("lake-tahoe-conditions")
 
+    today = datetime.datetime.now(datetime.timezone.utc)
     # TODO: possibly zip these files so we only need to send one request
     for dir in OUTPUT_DIRS:
-        for filename in os.listdir(dir):
-            # Send file to backend server if file's timestamp is greater than today
-            # Parse timestamp from file
-            file_date = datetime.datetime.strptime(filename, "%Y-%m-%d %H.npy")
-            # Sets timezone to UTC without affecting other values
-            file_date = file_date.replace(tzinfo=datetime.timezone.utc)
-            if file_date > today:
-                # Determine object key
-                if dir.endswith("flow"):
-                    objKey = f"flow/{filename}"
-                else:
-                    objKey = f"temperature/{filename}"
+        for fileName in os.listdir(dir):
+            fileDate = datetime.datetime.strptime(fileName, "%Y-%m-%d %H.npy")
+            # TODO: use PST to avoid confusion?
+            fileDate = fileDate.replace(tzinfo=datetime.timezone.utc)
+            if fileDate <= today:
+                # Assume old predictions already in S3
+                continue
 
-                # Upload file to s3
-                filePath = f"{dir}/{filename}"
-                # TODO: handle upload errors
-                bucket.upload_file(filePath, objKey)
+            # Upload new prediction to S3
+            # Determine object key
+            if dir.endswith("flow"):
+                objKey = f"flow/{fileName}"
+            else:
+                objKey = f"temperature/{fileName}"
+
+            filePath = f"{dir}/{fileName}"
+            # TODO: handle upload errors
+            bucket.upload_file(filePath, objKey)
 
 if __name__ == '__main__':
     save_model_output()
